fc1206a7f9d0e63de74b6e1711894455e82781b1
Dejw7n <dejw7n@gmail.com>
2022-04-24 11:56:29 +0200

    Change style rounded to lg

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { HomeComponent } from "./modules/home/home/home.component";
+import { LearningPointsComponent } from "./modules/learning-points/learning-points/learning-points.component";
 
-const routes: Routes = [{ path: "", component: HomeComponent }];
+const routes: Routes = [
+	{ path: "", component: HomeComponent },
+	{ path: "learning-points", component: LearningPointsComponent },
+];
 
 @NgModule({
 	imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 
 import { HomeModule } from "./modules/home/home.module";
+import { LearningPointsComponent } from "./modules/learning-points/learning-points/learning-points.component";
 
 @NgModule({
-	declarations: [AppComponent],
+	declarations: [AppComponent, LearningPointsComponent],
 	imports: [BrowserModule, AppRoutingModule, HomeModule],
 	providers: [],
 	bootstrap: [AppComponent],
